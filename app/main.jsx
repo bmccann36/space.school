042f063904ab5c4a0bc1cb88ed628c824ac98a96
Brian McCann <Brian@Users-MBP.home>
2017-10-16 08:40:30 -0400

    starting to seperate stateful and stateless

--- app/main.jsx
+++ app/main.jsx
@@ -7,6 +7,7 @@ import { HashRouter, Route, Link } from 'react-router-dom';
 
 import store from './store'
 import Root from './components/Root'
+import Nav from 'components./Nav'
 import StudentsHome from './components/StudentsHome'
 import Home from './components/Home'
 
@@ -14,18 +15,11 @@ render(
   <Provider store={store}>
     <HashRouter>
       <div className= "main">
-          <nav>
-            <div className="nav-item">
-              <a href="#">home</a>
-            </div>
-            <div className="nav-item">
-              <Link to= '/students' >students </Link>
-            </div>
-          </nav>
-        <div className = "body">
-           <Route exact path='/' component={Home} />
+
+          <Route path='/' component={Nav} />
+          <Route path='/' component={Home} />
           <Route exact path='/students' component={StudentsHome} />
-        </div>
+
       </div>
      </HashRouter>
   </Provider>,
